9d65090d0960f8c14712ba0268e9a3887b4a7cde
Din Fridman <dinfridman@Dins-MacBook-Pro.local>
2024-01-24 16:57:07 +0200

    right arrow icon added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-
 import CustomButton from './components/CustomButton/CustomButton';
+import RightArrowIcon from './images/Arrow - Right.svg'
 
 function App() {
   const customButtonProps = {
     styles: { fontSize: "2rem", fontFamily: "Roboto" },
     onClick: () => console.log('Button clicked'),
     disabled: false,
-    variant: "contained" as "contained"
+    variant: "contained" as "contained",
+    endIcon: <img src={RightArrowIcon} alt="Right Arrow" />
   };
 
   return (
--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -4,13 +4,15 @@ import Button, { ButtonProps as MuiButtonProps }from '@mui/material/Button';
 export interface CustomButtonProps extends MuiButtonProps {
   styles?: React.CSSProperties;
   children?: ReactNode;
+  endIcon?: ReactNode;
 };
 
-function CustomButton({styles , children, ...restProps } : CustomButtonProps) {
+function CustomButton({styles , children,endIcon, ...restProps } : CustomButtonProps) {
    
   return (
     <Button 
       sx={styles}
+      endIcon={endIcon}
       {...restProps}
     >
         {children}
